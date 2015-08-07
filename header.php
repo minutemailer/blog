9aef2166111924a735412dbe54779d3c66e16b6d
Tobias Bleckert <hola@tobiasbleckert.se>
2015-08-07 02:43:05 +0200

    Base template code

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+	
+	<head>
+		<meta name="viewport" content="width=device-width">
+		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+		<script>document.documentElement.className = '';</script>
+		<?php wp_head(); ?>
+	</head>
+
+	<body <?php body_class(); ?>>
