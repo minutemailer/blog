55578f7c6cd1cd80a3be875b7a6c1a006ca17bc1
Tobias Bleckert <hola@tobiasbleckert.se>
2015-08-07 02:43:49 +0200

    Config to disable emojis

--- config.php
+++ config.php
@@ -3,3 +3,6 @@
 /** Paths */
 define( 'DOCROOT', get_template_directory() . '/' );
 define( 'WEBROOT', get_stylesheet_directory_uri() . '/' );
+
+/** Disable Emojis */
+define( 'DISABLE_EMOJIS', TRUE );
--- functions.php
+++ functions.php
@@ -2,3 +2,9 @@
 
 /** Config */
 include TEMPLATEPATH . '/config.php';
+
+/** Disable emojis? */
+if ( defined( 'DISABLE_EMOJIS' ) && DISABLE_EMOJIS ) {
+	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+}
