26e865ab244629ace5e1d122184b8d5473b40c6b
Tobias Bleckert <hola@tobiasbleckert.se>
2015-08-08 15:41:34 +0200

    Dev version

--- config.php
+++ config.php
@@ -3,6 +3,9 @@
 /** GLOBALS */
 define( 'LANG_DOMAIN', 'timetomarket' );
 
+/** DEV Version */
+define( 'VERSION', time() );
+
 /** Paths */
 define( 'DOCROOT', get_template_directory() . '/' );
 define( 'WEBROOT', get_stylesheet_directory_uri() . '/' );
