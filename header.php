e0fcfa908b9b41fcd6c679a688d3138ec8182b29
Tobias Bleckert <hola@tobiasbleckert.se>
2015-08-08 15:09:56 +0200

    Header

--- header.php
+++ header.php
@@ -10,3 +10,17 @@
 	</head>
 
 	<body <?php body_class(); ?>>
+		<header class="main-header">
+			<div class="wrapper">
+				<h1 class="main-header__logo logo">
+					<?php echo TimeToMarket::logo(); ?>
+				</h1>
+				
+				<?php $items = TimeToMarket::menu_items( 'main-nav' ); if ( $items ): ?>
+				<nav class="nav main-header__nav">
+					<?php foreach ( $items as $item ): ?>
+					<?php endforeach; ?>
+				</nav>
+				<?php endif; ?>
+			</div>
+		</header>
