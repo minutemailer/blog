6edd067e97dc627502567a2776867ef2b9ec2d61
Tobias Bleckert <hola@tobiasbleckert.se>
2015-08-13 20:39:30 +0200

    Post author

--- single.php
+++ single.php
@@ -11,6 +11,23 @@
 				<div class="articles__article__body">
 					<?php the_content(); ?>
 				</div>
+
+				<figure class="author">
+					<div class="author__photo">
+						<?php echo get_avatar( get_the_author_meta( 'ID' ), 65, NULL, '', ['class' => 'author__img'] ); ?>
+					</div>
+
+					<div class="author__content">
+						<strong class="author__name"><?php the_author(); ?></strong>
+						<div class="author__bio">
+							<?php echo apply_filters( 'the_content', get_the_author_meta( 'description' ) ); ?>
+						</div>
+
+						<figcaption class="author__caption">
+							<a class="author__more" href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><?php echo sprintf( __( 'View all posts by %s', 'timetomarket' ), get_the_author() ); ?></a>
+						</figcaption>
+					</div>
+				</figure>
 				<?php endwhile; ?>
 			</div>
 		</div>
